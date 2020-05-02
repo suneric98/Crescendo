d6ba60bdd8873644c5e2d1267a337799491ee8b3
Eric Sun <suneric98@gmail.com>
2020-05-02 01:51:39 -0400

    trying to get bootstrap to work

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,17 +1,23 @@
 import json
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 import pandas as pd
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/line_graph/")
+def line_graph():
     df = pd.read_csv('topChartWithFeatures.csv')
     chart_data = df.to_dict(orient='records')
     chart_data = json.dumps(chart_data, indent=2)
     data = {'chart_data': chart_data}
-    return render_template("index.html", data=data)
+    return render_template("line_graph.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
